1e4f6d1466ebcaef74fdacc9d95a3ffa2bc96859
Michel Lima <michellima66@gmail.com>
2020-06-23 23:20:31 -0300

    Criar o projeto

---
+++ src/main/java/com/example/demo/Av3projectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Av3projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Av3projectApplication.class, args);
+	}
+
+}
