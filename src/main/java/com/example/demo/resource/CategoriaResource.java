42f066a873d1ac426b0a6b4c3744ead99d7efd48
Michel Lima <michellima66@gmail.com>
2020-06-24 01:44:07 -0300

    Configuração do banco de dados

--- src/main/java/com/example/demo/resource/CategoriaResource.java
+++ src/main/java/com/example/demo/resource/CategoriaResource.java
@@ -17,10 +17,14 @@ public class CategoriaResource {
 	public List<Contato> listar() {
 		Contato contato1 = new Contato(1, "Joao","99999999","joao@yyahoo.com");
 		Contato contato2 = new Contato(2, "Maria","88888888","maria@yyahoo.com");
+		Contato contato3 = new Contato(3, "Marta","86679883","marta@yyahoo.com");
+		Contato contato4 = new Contato(4, "Camila","54326549","camila@yyahoo.com");
 		
 		List<Contato> contatos = new ArrayList<>();
 		contatos.add(contato1);
 		contatos.add(contato2);
+		contatos.add(contato3);
+		contatos.add(contato4);
 		
 		return contatos;
 	}
