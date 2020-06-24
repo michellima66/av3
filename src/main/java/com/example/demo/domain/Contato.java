b06d8afda2bf3509b3cd8695a62480715b34a753
Michel Lima <michellima66@gmail.com>
2020-06-23 23:50:48 -0300

    Criar classe contato

---
+++ src/main/java/com/example/demo/domain/Contato.java
@@ -0,0 +1,80 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+
+public class Contato implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	private String telefone;
+	private String email;
+	
+	public Contato() {
+	}
+	
+	public Contato(int id, String nome, String telefone, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+}
